41a68f98d1df4b2354978cefe4187de8395b4098
blurname <naughtybao@outlook.com>
2022-06-02 07:16:20 +0000

    BUMP: flush config

--- type/index.ts
+++ type/index.ts
@@ -19,3 +19,30 @@ const safeStringDict: SafeDictionary<string, string> ={
   123: 's'
 }
 //console.log(safeStringDict.)
+//
+
+type PropEventSource<T> = {
+    on<K extends string & keyof T>
+        (eventName: `${K}Changed`, callback: (newValue: T[K]) => void ): void;
+};
+ 
+declare function makeWatchedObject<T>(obj: T): T & PropEventSource<T>;
+ 
+let person = makeWatchedObject({
+    firstName: "Homer",
+    age: 42,
+    location: "Springfield",
+});
+ 
+// works! 'newName' is typed as 'string'
+person.on("firstNameChanged", newName => {
+    // 'newName' has the type of 'firstName'
+    console.log(`new name is ${newName.toUpperCase()}`);
+});
+ 
+// works! 'newAge' is typed as 'number'
+person.on("ageChanged", newAge => {
+    if (newAge < 0) {
+        console.log("warning! negative age");
+    }
+})

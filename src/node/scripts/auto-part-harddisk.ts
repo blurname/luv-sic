9f626c9c2a886bb12cec58d590a12e82a7658da2
blurname <naughtybao@outlook.com>
2022-06-19 15:29:34 +0000

    CHG: use core.ts to export node's function

--- src/node/scripts/auto-part-harddisk.ts
+++ src/node/scripts/auto-part-harddisk.ts
@@ -1,6 +1,4 @@
-import { exec } from 'node:child_process'
-import { promisify } from 'util'
-const pExec = promisify(exec)
+import { exec } from '../core'
 const partMountDrive = async () => {
   const sdx = process.argv[2]
   // the new first partion number is 1
@@ -8,23 +6,23 @@ const partMountDrive = async () => {
   const mountPoint = '~/data'
   const fstabPath = '/etc/fstab'
   // await pExec(`umount ${sdx1}`)
-  await pExec(`rm  -r ${mountPoint}`)
-  await pExec(`parted ${sdx} rm 1`)
-  await pExec(`mkdir ${mountPoint}`)
+  await exec(`rm  -r ${mountPoint}`)
+  await exec(`parted ${sdx} rm 1`)
+  await exec(`mkdir ${mountPoint}`)
 
   //await pExec(`sudo parted ${sdx} mklabel gpt`)
-  await pExec(`parted ${sdx} -- mkpart primary ext4 1MiB 100%`)
+  await exec(`parted ${sdx} -- mkpart primary ext4 1MiB 100%`)
 
   // 3. mkfs
-  await pExec(`mkfs.ext4 ${sdx1}`)
+  await exec(`mkfs.ext4 ${sdx1}`)
 
   // await pExec(`mount ${sdx1} ${mountPoint}`)
 
   // 4. append to fstab
-  const { stdout: UUIDmessage } = await pExec(`blkid ${sdx1}`)
+  const { stdout: UUIDmessage } = await exec(`blkid ${sdx1}`)
   const UUID = UUIDmessage.split(' ')[1].split('=')[1].split("\"")[1]
   const appendMessage = `\n# ${sdx1}\nUUID=${UUID} /home/mb/data ext4 rw,relatime 0 2`
-  await pExec(`echo "${appendMessage}" >> ${fstabPath} `)
+  await exec(`echo "${appendMessage}" >> ${fstabPath} `)
 
 }
 

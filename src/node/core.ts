e6a80a9fe3d77d37289201811408a56681a5e034
blurname <naughtybao@outlook.com>
2022-06-24 04:21:21 +0000

    ADD: type KeyCode

--- src/node/core.ts
+++ src/node/core.ts
@@ -1,10 +1,10 @@
 import { exec } from 'node:child_process'
-// import { stat } from 'node:'
-import { stat, access } from 'node:fs/promises'
+import { stat, access, rename } from 'node:fs/promises'
 import { promisify } from 'util'
 const pExec = promisify(exec)
 export {
   pExec as exec,
   stat,
-  access
+  access,
+  rename
 }

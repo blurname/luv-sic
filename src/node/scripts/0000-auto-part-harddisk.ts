838d3552f200c3a9d534d4f4613ec09d5825e57d
blurname <naughtybao@outlook.com>
2022-06-25 08:42:06 +0000

    CHG: mount harddisk in /mnt/data

--- src/node/scripts/0000-auto-part-harddisk.ts
+++ src/node/scripts/0000-auto-part-harddisk.ts
@@ -1,22 +1,25 @@
 import { exec } from '../core'
+// use sudo to execute this script
 const partMountDrive = async () => {
+
   const sdx = process.argv[2]
   // the new first partion number is 1
   const sdx1 = sdx + '1'
-  const mountPoint = '~/data'
+  const mountPoint = '/mnt/data'
   const fstabPath = '/etc/fstab'
-  // await pExec(`umount ${sdx1}`)
-  await exec(`rm  -r ${mountPoint}`)
-  await exec(`parted ${sdx} rm 1`)
   await exec(`mkdir ${mountPoint}`)
 
-  //await pExec(`sudo parted ${sdx} mklabel gpt`)
+  //await exec(`rm  -r ${mountPoint}`)
+  await exec(`parted ${sdx} rm 1`)
+
+  //await exec(`sudo parted ${sdx} mklabel gpt`)
   await exec(`parted ${sdx} -- mkpart primary ext4 1MiB 100%`)
 
   // 3. mkfs
   await exec(`mkfs.ext4 ${sdx1}`)
 
-  // await pExec(`mount ${sdx1} ${mountPoint}`)
+  await exec(`mount ${sdx1} ${mountPoint}`)
+  await exec (`chmod 777 ${mountPoint}`)
 
   // 4. append to fstab
   const { stdout: UUIDmessage } = await exec(`blkid ${sdx1}`)

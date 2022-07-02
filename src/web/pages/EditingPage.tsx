908a9e040133365b21081333f872c4fee2c92185
blurname <naughtybao@outlook.com>
2022-07-02 03:27:45 +0000

    FIX: set css style incorrectly

--- src/web/pages/EditingPage.tsx
+++ src/web/pages/EditingPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { clickListener, inputListener } from '../sdk/event'
-export function EditingPage () {
-  const [ TIValue, setTIValue ] = useState('')
+export function EditingPage() {
+  const [TIValue, setTIValue] = useState('')
   const TIRef = useRef<HTMLTextAreaElement>(null)
   const TORef = useRef<HTMLTextAreaElement>(null)
   const randomRGBA = () => {
@@ -13,23 +13,23 @@ export function EditingPage () {
     }
   }
 
-  const handleTIInputing = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleTIInputing = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTIValue(e.target.value)
     const rgba = randomRGBA()
     const rgba2 = randomRGBA()
-    TIRef.style = `flex:1; background: rgba(${rgba.r},${rgba.g},${rgba.a},${rgba.a}) `
-    TORef.style = `flex:1; background: rgba(${rgba2.r},${rgba2.g},${rgba2.a},${rgba2.a}) `
+    TIRef.current!.style.setProperty('background:',` rgba(${rgba.r},${rgba.g},${rgba.a},${rgba.a}) `)
+    TORef.current!.style.setProperty('background',`rgba(${rgba2.r},${rgba2.g},${rgba2.a},${rgba2.a}) `)
   }
 
-  const handleTIClicking = (ref:HTMLTextAreaElement) => (e:React.MouseEvent<HTMLDivElement>) => {
+  const handleTIClicking = (ref: HTMLTextAreaElement) => (e: React.MouseEvent<HTMLDivElement>) => {
     const rgba = randomRGBA()
-    ref.style = `flex:1; background: rgba(${rgba.r},${rgba.g},${rgba.a},${rgba.a}) `
+    ref.style.setProperty('background', ` rgba(${rgba.r},${rgba.g},${rgba.a},${rgba.a} `)
   }
 
   useEffect(() => {
-    inputListener(TIRef.current, handleTIInputing)
-    clickListener(TIRef.current, handleTIClicking)
-    clickListener(TORef.current, handleTIClicking)
+    inputListener(TIRef.current!, handleTIInputing)
+    clickListener(TIRef.current!, handleTIClicking)
+    clickListener(TORef.current!, handleTIClicking)
   }, [])
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>

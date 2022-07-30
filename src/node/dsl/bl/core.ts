c9eae65f3f26af59692ca0b548374fa2001cb8aa
blurname <naughtybao@outlook.com>
2022-07-30 19:23:45 +0800

    WIP: b lang

---
+++ src/node/dsl/bl/core.ts
@@ -0,0 +1,73 @@
+type Query = {
+  type: "query";
+};
+type Add = {
+  type: "add";
+};
+type Delete = {
+  type: "delete";
+};
+type Update = {
+  type: "update";
+};
+type Parens = {
+  type: "parens";
+  value: "(" | ")";
+};
+type Result = {
+  type: "result";
+  value: any;
+};
+type From = {
+  type: "from";
+};
+type Where = {
+  type: "where";
+};
+type WhereExpression = {
+  type: "whereExpression";
+  value:
+    | `${string} >= ${string}`
+    | `${string} === ${string}`
+    | `${string} <= ${string}`;
+};
+type Expression = {
+  type: "expression";
+};
+
+type Command = Add | Delete | Update;
+
+type Token = Query | Command | Parens | Result | From | Where | WhereExpression;
+const Tokenizer = (input: String) => {
+  const tokens: Token[] = [];
+  const source = input.split(" ");
+  source.map((s) => {
+    if (s === "query") {
+      tokens.push({
+        type: "query",
+      });
+    } else if (s === "(") {
+      tokens.push({
+        type: "parens",
+        value: "(",
+      });
+    } else if (s === "result") {
+    } else if (s === ")") {
+      tokens.push({
+        type: "parens",
+        value: ")",
+      });
+    } else if (s === "from") {
+      tokens.push({
+        type: "from",
+      });
+    } else if (s === "where") {
+      tokens.push({
+        type: "where",
+      });
+    }
+  });
+};
+const parensTokenizer = (source: String[], tokens: Token[]) => {};
+const whereTokenizer = (source: String[], tokens: Token[]) => {};
+export {};

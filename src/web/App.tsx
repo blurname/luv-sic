bcc7591ebcbcd742309dff49f67d9072a58b0bc1
blurname <naughtybao@outlook.com>
2022-08-06 10:44:23 +0800

    CHG: lint

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
 import { EditingPage } from './pages/EditingPage'
-type Page =
-| 'Editing'
-function App () {
-  const [ activePage, setActivePage ] = useState<Page>('Editing')
+type Page = 'Editing'
+function App() {
+  const [activePage, setActivePage] = useState<Page>('Editing')
   return (
     <div style={{ width: '100vw', height: '100vh', boxSizing: 'border-box' }} className="App">
-      {
-        activePage === 'Editing' ? <EditingPage />
-          : undefined
-      }
+      {activePage === 'Editing' ? <EditingPage /> : undefined}
     </div>
   )
 }
--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { RemeshRoot } from 'remesh-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RemeshRoot>
+      <App />
+    </RemeshRoot>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )

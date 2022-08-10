93e3f7145f431f37e4803b94919a994b89976c91
blurname <naughtybao@outlook.com>
2022-08-10 22:17:46 +0800

    WIP: rxjs

---
+++ src/node/test/performance.ts
@@ -0,0 +1,34 @@
+type Latency = {
+  value: number
+  latency: number
+  log: (v: number) => void
+}
+
+const action1: Latency = {
+  value: 1,
+  latency: 500,
+  log: (v) => {
+    console.log(v)
+  },
+}
+const action2: Latency = {
+  value: 1,
+  latency: 1500,
+  log: (v) => {
+    console.log(v)
+  },
+}
+const action3: Latency = {
+  value: 2,
+  latency: 1000,
+  log: (v) => {
+    console.log(v)
+  },
+}
+const currentLatencyMs = 100000
+const actionList: Latency[] = []
+const a = performance.now()
+setTimeout(() => {
+  const b = performance.now()
+  console.log(Math.round(b - a))
+}, 5000)
---
+++ src/node/test/rxjs.ts
@@ -0,0 +1,59 @@
+import { from, filter, switchMap, timeout, map, delay, of, first } from 'rxjs'
+type Latency = {
+  value: number
+  latency: number
+  log: (v: number) => void
+}
+
+const action1: Latency = {
+  value: 1,
+  latency: 4500,
+  log: (v) => {
+    console.log(v)
+  },
+}
+const action2: Latency = {
+  value: 2,
+  latency: 2000,
+  log: (v) => {
+    console.log(v)
+  },
+}
+const action3: Latency = {
+  value: 3,
+  latency: 1000,
+  log: (v) => {
+    console.log(v)
+  },
+}
+let currentLatencyMs = 100000
+const actionList: Latency[] = []
+actionList.push(action1)
+const oA = from(actionList).pipe(
+  filter((x) => {
+    if (x.latency < currentLatencyMs) {
+      currentLatencyMs = x.latency
+      return true
+    }
+    return false
+  }),
+  switchMap((x) => {
+    //delay(x.latency)
+    const a = of(x).pipe(delay(x.latency))
+    return a
+  }),
+  first(),
+)
+const subject = oA.subscribe()
+setTimeout(() => {
+  actionList.push(action2)
+  oA.subscribe((x) => {
+    x.log(x.value)
+  })
+}, 1000)
+setTimeout(() => {
+  actionList.push(action3)
+  oA.subscribe((x) => {
+    x.log(x.value)
+  })
+}, 1500)

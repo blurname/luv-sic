6f582c1e590ed883a98b713bb334a8e5d3b3a75a
blurname <naughtybao@outlook.com>
2022-08-13 17:11:46 +0800

    CHG: rename scripts & add arg in working time

--- src/node/scripts/0005-compute-working-time.ts
+++ src/node/scripts/0005-compute-working-time.ts
@@ -1,12 +1,14 @@
 import { exec } from '../core'
-const calcWorkingTime = async () => {
+const computeWorkingTime = async () => {
+  const [path1, path2, _, inputTime] = process.argv
+  const realTime = inputTime ?? '09:00'
   const { stdout: time } = await exec('timedatectl status')
   const local = time.split('\n')[0]
   const dateIndex = local.indexOf(':')
   const stdTime = local.slice(dateIndex + 1).trim()
   const month = stdTime.split(' ')[1]
-  const beginTime = `${month} 09:00:00 CST`
+  const beginTime = `${month} ${realTime}:00 CST`
   const hours = ((new Date(stdTime).getTime() - new Date(beginTime).getTime()) / 1000 / 60 - 90 - 40) / 60
   console.log('working hours:', hours)
 }
-export { calcWorkingTime }
+export { computeWorkingTime }
--- src/node/scripts/main.ts
+++ src/node/scripts/main.ts
@@ -1,7 +1,7 @@
 import { switchNpmrc } from './0001-switch-npmrc'
 import { duCurrentFolder } from './0003-du-current-folder'
 import { calcTsPercentInProject } from './0004-calc-ts-percent-in-project'
-import { calcWorkingTime } from './0005-calc-working-time'
+import { computeWorkingTime } from './0005-calc-working-time'
 
 type WhichCommand = keyof typeof commands
 
@@ -9,7 +9,7 @@ export const commands = {
   switchNpmrc,
   duCurrentFolder,
   calcTsPercentInProject,
-  calcWorkingTime,
+  computeWorkingTime,
 }
 
 const main = () => {

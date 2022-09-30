26754278f5fdebda74c942d7b433ee877c6282f0
blurname <naughtybao@outlook.com>
2022-09-30 21:32:34 +0800

    ADD: cli utils

---
+++ src/node/cli.ts
@@ -0,0 +1,28 @@
+type KvMapFromScript = { [k: string]: any }
+
+const reduceDash = (strHasDash: `-${string}`) => {
+  return strHasDash.split('-').at(-1)
+}
+
+const parseOptionList = (argv: String[], kvMapFromScript: KvMapFromScript) => {
+  const parsedOptionList: KvMapFromScript = {}
+  Object.keys(kvMapFromScript).forEach((k) => {
+    const paramK = argv.findIndex((arg) => {
+      return reduceDash(arg) === k
+    })
+    if (paramK !== -1) {
+      const paramV = argv[paramK + 1]
+      parsedOptionList[k] = paramV
+    }
+  })
+  return { ...kvMapFromScript, ...parsedOptionList }
+}
+//export { parseOptionList }
+//
+//const main = () => {
+//const result = parseOptionList(process.argv, options)
+//console.log(result)
+//}
+//main()
+
+export { parseOptionList }

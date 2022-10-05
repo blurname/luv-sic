0cb12a087397307dca0c5efcc36d8f1c3e667c92
blurname <naughtybao@outlook.com>
2022-10-05 15:58:19 +0800

    ADD: script: working time endtime

--- src/node/scripts/05-compute-working-time.ts
+++ src/node/scripts/05-compute-working-time.ts
@@ -3,15 +3,23 @@ const computeWorkingTimeDesc = 'computing my Saturday working time'
 const computeWorkingTime = async () => {
   const [path1, path2, _, inputTime] = process.argv
   const realTime = inputTime ?? '09:00'
+
   const { stdout: time } = await exec('timedatectl status')
+
   const local = time.split('\n')[0]
   const dateIndex = local.indexOf(':')
   const stdTime = local.slice(dateIndex + 1).trim()
   const month = stdTime.split(' ')[1]
+
   const beginTime = `${month} ${realTime}:00 CST`
+  // minus 12:00 - 13:30, 6:00 - 6:40
+  const hours = ((new Date(stdTime).getTime() - new Date(beginTime).getTime()) / 1000 / 60 - 40 - 90) / 60
+  const endTime = new Date(new Date(beginTime).getTime() + 60 * 6 * 1000)
+
   console.log('beginTime:', beginTime)
-  console.log('stdTime:', stdTime)
-  const hours = ((new Date(stdTime).getTime() - new Date(beginTime).getTime()) / 1000 / 60 - 40) / 60
+  console.log('currentTime:', stdTime)
+  console.log('endTime', endTime)
+  console.log('minus 12:00 - 13:30, 6:00 - 6:40')
   console.log('working hours:', hours)
 }
 export { computeWorkingTime, computeWorkingTimeDesc }

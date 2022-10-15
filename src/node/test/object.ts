db22cb54e3223bcab81d33e5c291c5de97c964a5
blurname <naughtybao@outlook.com>
2022-10-15 09:57:55 +0800

    ADD: deepUpdate object

--- src/node/test/object.ts
+++ src/node/test/object.ts
@@ -10,6 +10,7 @@ const deepObject = {
     },
   },
 } as const
+
 const deepObject1 = {
   a: 1,
   b: {
@@ -75,13 +76,43 @@ const detectWhichPath = (obj, key): string => {
   return finalPath
 }
 
-const deepObjectUpdate = (obj, path, value) => {
-  const ss = path.split(':')
-  let current = obj[ss[0]]
-  if (current !== undefined) {
-    for (let i = 1; i < ss.length; i++) {
-      current = current[ss[i]]
+//const deepObjectUpdate = (obj, path, value) => {
+//const ss = path.split(':')
+//let current = obj[ss[0]]
+//if (current !== undefined) {
+//for (let i = 1; i < ss.length; i++) {
+//current = current[ss[i]]
+//}
+//}
+//}
+//
+//
+const deepObjectUpdate2 = (obj, targetKey, value, path) => {
+  const v = path.split(':')
+  const recUpdate = (currentObj, depth) => {
+    if (v[depth] === targetKey) {
+      return { ...currentObj, [targetKey]: value }
+    }
+    return {
+      ...currentObj,
+      [v[depth]]: {
+        ...recUpdate(obj[v[depth]], depth + 1),
+      },
     }
   }
+  const updated = recUpdate(obj, 0)
+  console.log(updated)
 }
 console.log(detectWhichPath(deepObject, 'ccc'))
+const finalPath = detectWhichPath(deepObject, 'ccc')
+console.log(finalPath)
+deepObjectUpdate2(deepObject, 'ccc', 'kk', finalPath)
+console.log(deepObject)
+
+//const cc = deepObject.c.cc.ccc
+////let ccc = cc.ccc
+////
+//deepObject.c.cc.ccc = 1
+
+//ccc = 1
+//console.log(deepObject)

b934b63809b1ca0581bedf88c816cecdea826dac
blurname <naughtybao@outlook.com>
2023-02-14 00:17:06 +0800

    FIX: comment `rename-files`

--- packages/cli/src/commands/02-rename-files.ts
+++ packages/cli/src/commands/02-rename-files.ts
@@ -1,10 +1,10 @@
-import { exec, rename } from '@blurname/core/src/core'
-const renameFile = async () => {
-  const [path1, path2, ...path3] = process.argv
-  console.log(path3)
-  const { stdout } = await exec('ls')
-  console.log(stdout)
-}
+//import { exec, rename } from '@blurname/core/src/core'
+//const renameFile = async () => {
+  //const [path1, path2, ...path3] = process.argv
+  //console.log(path3)
+  //const { stdout } = await exec('ls')
+  //console.log(stdout)
+//}
 // renameFile()
 // export {
 

af7aed2ba2fd7e42802fa1807c7473e28b8f4ccb
blurname <naughtybao@outlook.com>
2023-02-21 01:05:02 +0800

    FIX: svg-simpe: build config

--- packages/svgminify/vite.config.ts
+++ packages/svgminify/vite.config.ts
@@ -1,14 +1,14 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es'],
-    },
+    sourcemap: true,
     rollupOptions: {
-      external: /^lit/,
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
     },
   },
 })

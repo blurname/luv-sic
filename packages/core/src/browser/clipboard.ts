bb2e090e07be0eeea7fed92800d8c625d5311f9b
blurname <naughtybao@outlook.com>
2023-03-01 23:10:59 +0800

    ADD: core: clipboard

---
+++ packages/core/src/browser/clipboard.ts
@@ -0,0 +1,35 @@
+type ClipboardItem = {
+  mimeType: 'text/plain'
+  data: string
+}
+const performNativeCopy = (items: ClipboardItem[]): boolean => {
+  let success = false
+  const tempElem = document.createElement('textarea')
+  tempElem.value = 'temp'
+  document.body.appendChild(tempElem)
+  tempElem.select()
+  tempElem.setSelectionRange(0, tempElem.value.length)
+
+  const listener = (e: ClipboardEvent) => {
+    const clipboardData = e.clipboardData
+    if (clipboardData) {
+      items.forEach(item => clipboardData.setData(item.mimeType, item.data))
+    }
+
+    e.preventDefault()
+    e.stopPropagation()
+    tempElem.removeEventListener('copy', listener)
+  }
+
+  tempElem.addEventListener('copy', listener)
+  try {
+    success = document.execCommand('copy')
+  } finally {
+    tempElem.removeEventListener('copy', listener)
+    document.body.removeChild(tempElem)
+  }
+  return success
+}
+export {
+  performNativeCopy
+}

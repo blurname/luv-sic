2d727e8ccb9fbedf75cfff27e38984e1beea7207
blurname <naughtybao@outlook.com>
2023-03-04 11:22:59 +0800

    MISC: a string test

--- packages/play/node/test/stringTest.ts
+++ packages/play/node/test/stringTest.ts
@@ -1,9 +1,14 @@
 const testSubstring = () => {
-  const str1 = "abc copy b"
+  const str1 = 'abc copy b'
   const pos = str1.indexOf('copy')
   const str2 = str1.substring(0, pos)
   console.log(str2)
 }
+
+const colonSeparated2Camel = (ss:string) => {
+  return ss.split(':').map((s) => s.substring(0, 1).toUpperCase() + s.substring(1)).join('')
+}
+console.log(colonSeparated2Camel('entry:draft:linkToastShow:update'))
 export {
 
 }

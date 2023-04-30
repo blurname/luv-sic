fa0b9f55eef5f397dc4ef2e9385a9bed138b8e0e
blurname <naughtybao@outlook.com>
2023-04-30 10:21:47 +0800

    CHG: sort file

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,17 +1,9 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  }
+  plugins: [react()]
 })

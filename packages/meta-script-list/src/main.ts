151c24eb72f193097679593d432a0a5268180063
blurname <naughtybao@outlook.com>
2023-06-14 13:17:33 +0800

    wip

--- packages/meta-script-list/src/main.ts
+++ packages/meta-script-list/src/main.ts
@@ -1,25 +1,69 @@
+import * as readline from 'node:readline'
 import { getPackageJsonFile } from '@blurname/core/src/node/meta-file/npm'
-import { execSync } from 'node:child_process'
+import { exec, execSync } from 'node:child_process'
 import { Fzf } from 'fzf'
+import { colorLog } from '@blurname/core/src/colorLog'
 const main = async () => {
   const pwd = execSync('pwd').toString().split('\n')[0]
-  // console.log(pwd)
   const rootPackageJsonPath = pwd + '/package.json'
-  console.log(rootPackageJsonPath)
   const jsonFileKit = getPackageJsonFile(rootPackageJsonPath)
   const packageJson = JSON.parse(jsonFileKit.getFileContent()) as {scripts:Record<string, string>}
   const scriptKeyList:string[] = []
   const scriptDescList:string[] = []
-  console.log(packageJson)
+  const keyDescMap = new Map(Object.entries(packageJson.scripts))
   Object.entries(packageJson.scripts).forEach(([k, v]) => {
     scriptKeyList.push(k)
     scriptDescList.push(v)
   })
-  console.log(scriptKeyList)
-  console.log(scriptDescList)
   const fzf = new Fzf(scriptKeyList)
-  const entries = fzf.find('dev')
-  entries.forEach(entry => console.log(entry.item))
+  readline.emitKeypressEvents(process.stdin)
+  const inputList:string[] = []
+  let resultList: string[] = []
+  let selectIndex = 0
+  let selectKey:string
+  if (process.stdin.isTTY) { process.stdin.setRawMode(true) }
+  process.stdin.on('keypress', (str, key:{name:string, ctrl:boolean}) => {
+    if (key.ctrl === true && key.name === 'c') {
+      process.exit()
+    }
+    if (key.name === 'return') {
+      exec(`npm run ${selectKey}`)
+      process.exit()
+    }
+
+    if (key.name === 'backspace') {
+      inputList.pop()
+      selectIndex = 0
+    } else if (key.name === 'up') {
+      if (selectIndex - 1 < 0) {
+        selectIndex = resultList.length - 1
+      } else {
+        selectIndex = (selectIndex - 1) % resultList.length
+      }
+    } else if (key.name === 'down') {
+      selectIndex = (selectIndex + 1) % resultList.length
+    } else {
+      inputList.push(str)
+      selectIndex = 0
+    }
+    const inputStr = inputList.join('')
+    const entries = fzf.find(inputStr)
+    resultList = []
+    console.clear()
+    console.log(inputStr)
+    let result = ''
+    entries.forEach((entry:{item:string}, index:number) => {
+      resultList.push(entry.item)
+      if (index === selectIndex) {
+        selectKey = entry.item
+        result += colorLog({ msg: `${entry.item}:${keyDescMap.get(entry.item)}\n`, fg: 'Green' })
+      } else {
+        result += `${entry.item}:${keyDescMap.get(entry.item)}\n`
+      }
+    }
+    )
+    console.log(result)
+  })
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 main()
@@ -28,15 +72,3 @@ main()
 // const readline = require('readline');
 
 // console.log("Press any key")
-
-// readline.emitKeypressEvents(process.stdin);
-// if (process.stdin.isTTY)
-// process.stdin.setRawMode(true);
-
-// process.stdin.on('keypress', (str, key) => {
-// if(key.ctrl == true && key.name == 'c'){
-// process.exit()
-// }
-// console.log(str)
-// console.log(key)
-// })

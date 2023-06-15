ec32e370e41a69b75b4ec0a3d2c63408ca430161
blurname <naughtybao@outlook.com>
2023-06-15 22:10:05 +0800

    WIP: a

--- packages/meta-script-list/src/main.ts
+++ packages/meta-script-list/src/main.ts
@@ -1,34 +1,57 @@
 import * as readline from 'node:readline'
 import { getPackageJsonFile } from '@blurname/core/src/node/meta-file/npm'
-import { execSync } from 'node:child_process'
+import { spawnSync } from 'node:child_process'
 import { Fzf } from 'fzf'
 import { colorLog } from '@blurname/core/src/colorLog'
 const main = async () => {
-  // const pwd = execSync('pwd').toString().split('\n')[0]
+  const logFzfResult = ({ entries, inputStr, selectIndex }:{entries:{item:string}[], inputStr:string, selectIndex:number}) => {
+    let result = ''
+    entries.forEach((entry, index:number) => {
+      resultList.push(entry.item)
+      if (index === selectIndex) {
+        selectKey = entry.item
+        result += colorLog({ msg: `${entry.item}: ${keyDescMap.get(entry.item)}\n`, fg: 'Green' })
+      } else {
+        result += `${entry.item}: ${keyDescMap.get(entry.item)}\n`
+      }
+    }
+    )
+    const final = inputStr + '\n_______________\n' + result
+    console.clear()
+    console.log(final)
+  }
+
   const rootPackageJsonPath = process.cwd() + '/package.json'
   const jsonFileKit = getPackageJsonFile(rootPackageJsonPath)
   const packageJson = JSON.parse(jsonFileKit.getFileContent()) as {scripts:Record<string, string>}
+
   const scriptKeyList:string[] = []
   const scriptDescList:string[] = []
   const keyDescMap = new Map(Object.entries(packageJson.scripts))
+
   Object.entries(packageJson.scripts).forEach(([k, v]) => {
     scriptKeyList.push(k)
     scriptDescList.push(v)
   })
+
   const fzf = new Fzf(scriptKeyList)
   readline.emitKeypressEvents(process.stdin)
   const inputList:string[] = []
   let resultList: string[] = []
-  let selectIndex = 0
-  let selectKey:string
+
   if (process.stdin.isTTY) { process.stdin.setRawMode(true) }
+
+  const entries = fzf.find('')
+  let selectIndex = 0
+  let selectKey = scriptKeyList[0]
+  logFzfResult({ entries, inputStr: '', selectIndex: 0 })
+
   process.stdin.on('keypress', (str, key:{name:string, ctrl:boolean}) => {
     if (key.ctrl === true && key.name === 'c') {
       process.exit()
     }
     if (key.name === 'return') {
-      // console.log(execSync('ls').toString())
-      execSync('bash echo hihi')
+      spawnSync('npm', ['run', selectKey], { stdio: 'inherit' })
       process.exit()
     }
 
@@ -50,21 +73,7 @@ const main = async () => {
     const inputStr = inputList.join('')
     const entries = fzf.find(inputStr)
     resultList = []
-    console.clear()
-    let result = ''
-    entries.forEach((entry:{item:string}, index:number) => {
-      resultList.push(entry.item)
-      if (index === selectIndex) {
-        selectKey = entry.item
-        result += colorLog({ msg: `${entry.item}: ${keyDescMap.get(entry.item)}\n`, fg: 'Green' })
-      } else {
-        result += `${entry.item}: ${keyDescMap.get(entry.item)}\n`
-      }
-    }
-    )
-    const final = inputStr + '\n_______________\n' + result
-    // process.stdout.write("Type characters, 'x' to exit\n")
-    console.log(final)
+    logFzfResult({ entries, inputStr, selectIndex })
   })
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises

ea406597fa25c76c5f72758a486568e3ad70ff86
blurname <naughtybao@outlook.com>
2023-06-16 22:04:58 +0800

    ADD: script: detect version need to jump

---
+++ scripts/detect-sub-version.ts
@@ -0,0 +1,12 @@
+import { execSync } from 'node:child_process'
+const detectSubVersionNeedToUpdate = (subPkgList:string[]) => {
+  const recentVersionCommitHashIn30 = execSync('git log --oneline -30 | grep VERSION -m 1').toString().split(' ')[0]
+  const needToBumpPkgList = subPkgList.filter((pkgName) => {
+    return execSync(`git diff ${recentVersionCommitHashIn30} packages/${pkgName}`).toString() !== ''
+  })
+
+  return needToBumpPkgList
+}
+export {
+  detectSubVersionNeedToUpdate
+}

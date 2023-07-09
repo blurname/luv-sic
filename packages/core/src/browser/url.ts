c7f718364f78c24dfc7e6d7db79936e5ef205988
blurname <naughtybao@outlook.com>
2023-07-09 17:22:32 +0800

    FEAT: core: mount config to globalThis

--- packages/core/src/browser/url.ts
+++ packages/core/src/browser/url.ts
@@ -1,16 +1,22 @@
 
 type Fn = ()=> any
-const createUrlInit = <const T extends Record<string, Fn>> (config:T) => (param?: keyof T & string) => {
-  const params = new URLSearchParams(location.search)
-  if (param === undefined) {
-    for (const k of params.keys()) {
-      if (typeof config[k] === 'function') { return config[k]() }
+const createUrlInit = <const T extends Record<string, Fn>> (config:T) => {
+  // write to globalThis, know the number of url parameter used
+  // @ts-ignore
+  Object.assign(globalThis, { _urlp: { ...globalThis._urlp, ...config } })
+
+  return (param?: keyof T & string) => {
+    const params = new URLSearchParams(location.search)
+    if (param === undefined) {
+      for (const k of params.keys()) {
+        if (typeof config[k] === 'function') { return config[k]() }
+      }
+      return
     }
-    return
-  }
-  const v = params.get(param)
-  if (v !== null) {
-    return config[param]()
+    const v = params.get(param)
+    if (v !== null) {
+      return config[param]()
+    }
   }
 }
 export { createUrlInit }

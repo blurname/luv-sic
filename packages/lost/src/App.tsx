e3410d3f1c777225f09bb246a5c9035b9109e535
blurname <naughtybao@outlook.com>
2023-07-15 15:33:47 +0800

    FEAT: init randomPress

--- packages/lost/src/App.tsx
+++ packages/lost/src/App.tsx
@@ -1,14 +1,10 @@
-import React, { useRef } from 'react'
-// import { EditingPage } from './pages/EditingPage'
-// import { Elevator } from './pages/Elevator'
-// type Pages = 'editing' | 'elevator'
-import { Lock } from './pages/Lock'
+import React from 'react'
+import { RandomPress } from './pages/RandomPress'
 function App () {
   // const [activePage, setActivePage] = useState<Pages>('elevator')
-  const divRef = useRef(null)
   return (
     <div style={{ width: '100vw', height: '100vh', boxSizing: 'border-box' }} className="App">
-      <Lock />
+      <RandomPress />
     </div>
   )
 }
---
+++ packages/lost/src/pages/RandomPress.tsx
@@ -0,0 +1,87 @@
+import { createUrlInit } from '@blurname/core/src/browser/url'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useRefresh } from '../hooks/useRefresh'
+import { fromEvent } from 'rxjs'
+
+const urlConfig = {
+  'dt': () => ({
+    background: '#0556a0',
+    color: '#e7f6ff'
+  }),
+  'mb': () => ({
+    background: '#f5f5f5',
+    color: '#101010'
+  }),
+  'night': () => ({
+    background: '#101010',
+    color: '#f5f5f5',
+    filter: 'brightness(20%)'
+  })
+}
+// TODO
+// 1. press chun, xia, qiu, dong to change text
+// 2. add animation for
+
+const t0 = '东西南北    宛尔目前    春夏秋冬    生于笔下'
+const t1 = '好雨知时节    当春乃发生'
+const t2 = '晚风吻尽荷花叶    让我醉到在池边'
+const t3 = '还没好好的感受    雪花绽放的气候'
+const t4 = '北国风光    千里冰封    万里雪飘'
+const tList = [t1, t2, t3, t4]
+const urlInit = createUrlInit(urlConfig)
+const style = urlInit()
+
+const RandomPress = () => {
+  const [screenKey, setScreenKey] = useState('')
+  const [isPressed, setIsPressed] = useState(false)
+
+  useEffect(() => {
+    const aaa = fromEvent(document, 'keyup')
+      .subscribe((ke) => {
+        console.log(screenKey)
+        setScreenKey(ke.key)
+        if (!isPressed) {
+          setIsPressed(true)
+        }
+      })
+    return () => {
+      aaa.unsubscribe()
+    }
+  }, [isPressed])
+
+  const res = screenKey.charCodeAt(0)
+  let renderText = ''
+  if (screenKey === 'Escape') {
+    renderText = t0
+  } else {
+    renderText = isPressed ? tList[res % 4] : t0
+  }
+  return (
+    <StyledLock className="lock" style={style}>
+      <div>{renderText}</div>
+    </StyledLock>
+  )
+}
+
+const StyledLock = styled.div`
+  width: 100%;
+  height: 100%;
+  font-size: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 100;
+  .time {
+    text-align: right;
+  }
+  .text {
+    font-size: 20px;
+    margin-left: 50px;
+    writing-mode: vertical-rl;
+  }
+`
+
+export {
+  RandomPress
+}

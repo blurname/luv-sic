f734cab651a2036eb99dc2b1a5266d3c1ce44b8b
blurname <naughtybao@outlook.com>
2023-08-08 22:01:42 +0800

    ADD: phase2

--- packages/lost/src/pages/RandomPress.tsx
+++ packages/lost/src/pages/RandomPress.tsx
@@ -29,53 +29,60 @@ const t1 = '好雨知时节    当春乃发生'
 const t2 = '晚风吻尽荷花叶    让我醉到在池边'
 const t3 = '已惯天涯莫浪愁，寒云衰草渐成秋'
 const t4 = '还没好好的感受    雪花绽放的气候'
-const tList = [t1, t2, t3, t4]
 const urlInit = createUrlInit(urlConfig)
 const style = urlInit()
 
-const chun = ['c', 'h', 'u', 'n']
-const xia = ['x', 'i', 'a']
-const qiu = ['q', 'i', 'u']
-const dong = ['d', 'o', 'n', 'g']
+const chun = 'chun'
+const xia = 'xia'
+const qiu = 'qiu'
+const dong = 'dong'
+
+const tMap = {
+  [chun]: t1,
+  [xia]: t2,
+  [qiu]: t3,
+  [dong]: t4
+}
 
 const RandomPress = () => {
-  const [screenKey, setScreenKey] = useState('')
-  const [isPressed, setIsPressed] = useState(false)
+  // const [screenKey, setScreenKey] = useState('')
+  // const [isPressed, setIsPressed] = useState(false)
+  const [res, setRes] = useState('')
 
   useEffect(() => {
     const keyDown$ = fromEvent(document, 'keydown')
     // 使用buffer操作符将连续的按键事件缓存为数组
     const keyCombination$ = keyDown$
       .pipe(
-        filter((event: KeyboardEvent) => targetKeys.includes(event.key)), // 过滤目标按键
+        // filter((event: KeyboardEvent) => targetKeys.includes(event.key)), // 过滤目标按键
         buffer(keyDown$.pipe(debounceTime(300))), // 在最后一个按键后等待 300ms 触发缓存
         // filter((event: KeyboardEvent) => event.key === targetKeys[0]), // 监听第一个按键
         // debounceTime(1000),
         // bufferCount(3), // 在 300ms 内连续按下的按键都会被缓存
         filter((keys: KeyboardEvent[]) => {
-          console.log(keys)
-          return keys.length === targetKeys.length
-        }), // 确保按键数量与目标组合键一致
-        filter((keys: KeyboardEvent[]) => {
-          console.log('keys', keys)
-          for (let i = 1; i < keys.length; i++) {
-            if (keys[i].key !== targetKeys[i]) {
-              return false
-            }
-          }
-          return true
-        })
+          const combo = keys.map(keys => keys.key).join('')
+          console.log('pre', combo)
+          return combo === 'chun' || combo === 'xia' || combo === 'qiu' || combo === 'dong'
+          // console.log(keys)
+          // return keys.length === targetKeys.length
+        }) // 确保按键数量与目标组合键一致
       )
     // 订阅组合键事件
-    const sub = keyCombination$.subscribe(combo => {
-      console.log('combo', combo)
+    const sub = keyCombination$.subscribe(comboP => {
+      // console.log('combo', combo, )
+      const combo = comboP.map(keys => keys.key).join('')
+
       if (combo === 'chun') {
+        setRes('chun')
         console.log('chun')
-      } else if (combo.indexOf('xia') !== -1) {
+      } else if (combo === 'xia') {
+        setRes('xia')
         console.log('xia')
-      } else if (combo.indexOf('qiu') !== -1) {
+      } else if (combo === 'qiu') {
+        setRes('qiu')
         console.log('qiu')
       } else if (combo === 'dong') {
+        setRes('dong')
         console.log('dong')
       }
     })
@@ -84,13 +91,14 @@ const RandomPress = () => {
     }
   }, [])
 
-  const res = screenKey.charCodeAt(0)
-  let renderText = ''
-  if (screenKey === 'Escape') {
-    renderText = t0
-  } else {
-    renderText = isPressed ? tList[res % 4] : t0
-  }
+  // const res = screenKey.charCodeAt(0)
+  const renderText = tMap[res] || t0
+
+  // if (screenKey === 'Escape') {
+  //   renderText = t0
+  // } else {
+  //   renderText = isPressed ? tList[res % 4] : t0
+  // }
   return (
     <StyledLock className="lock" style={style}>
       <div>{renderText}</div>

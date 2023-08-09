d29c644a5081414949eca1f914eef2ed056a23b6
blurname <naughtybao@outlook.com>
2023-08-09 21:52:09 +0800

    ADD: phase3

---
+++ packages/lost/src/misc/urlConfig.ts
@@ -0,0 +1,18 @@
+const themeConfig = {
+  'dt': () => ({
+    background: '#0556a0',
+    color: '#e7f6ff'
+  }),
+  'mb': () => ({
+    background: '#f5f5f5',
+    color: '#101010'
+  }),
+  'night': () => ({
+    background: '#101010',
+    color: '#f5f5f5',
+    filter: 'brightness(20%)'
+  })
+}
+export {
+  themeConfig
+}
--- packages/lost/src/pages/RandomPress.tsx
+++ packages/lost/src/pages/RandomPress.tsx
@@ -1,36 +1,18 @@
 import { createUrlInit } from '@blurname/core/src/browser/url'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { useRefresh } from '../hooks/useRefresh'
 import { fromEvent } from 'rxjs'
-import { buffer, filter, map, debounceTime, bufferCount, scan, take, switchMap } from 'rxjs/operators'
+import { buffer, debounceTime } from 'rxjs/operators'
+import { themeConfig } from '../misc/urlConfig'
 
-const urlConfig = {
-  'dt': () => ({
-    background: '#0556a0',
-    color: '#e7f6ff'
-  }),
-  'mb': () => ({
-    background: '#f5f5f5',
-    color: '#101010'
-  }),
-  'night': () => ({
-    background: '#101010',
-    color: '#f5f5f5',
-    filter: 'brightness(20%)'
-  })
-}
-// TODO
-// 1. press chun, xia, qiu, dong to change text
-// 2. add animation for
+// 1. press chun, xia, qiu, dong to change text +
+// 2. add animation
 
-const t0 = '东西南北    宛尔目前    春夏秋冬    生于笔下'
-const t1 = '好雨知时节    当春乃发生'
-const t2 = '晚风吻尽荷花叶    让我醉到在池边'
-const t3 = '已惯天涯莫浪愁，寒云衰草渐成秋'
-const t4 = '还没好好的感受    雪花绽放的气候'
-const urlInit = createUrlInit(urlConfig)
-const style = urlInit()
+const t0 = '东西南北 宛尔目前 春夏秋冬 生于笔下'
+const t1 = '好雨知时节 当春乃发生'
+const t2 = '晚风吻尽荷花叶 让我醉到在池边'
+const t3 = '已惯天涯莫浪愁 寒云衰草渐成秋'
+const t4 = '还没好好的感受 雪花绽放的气候'
 
 const chun = 'chun'
 const xia = 'xia'
@@ -44,9 +26,15 @@ const tMap = {
   [dong]: t4
 }
 
+const urlInit = createUrlInit(themeConfig)
+const style = urlInit()
+
+const isMeetSeason = (combo:string) => {
+  const season = [chun, xia, qiu, dong].filter((s) => { return s === combo })[0]
+  return season
+}
+
 const RandomPress = () => {
-  // const [screenKey, setScreenKey] = useState('')
-  // const [isPressed, setIsPressed] = useState(false)
   const [res, setRes] = useState('')
 
   useEffect(() => {
@@ -54,51 +42,24 @@ const RandomPress = () => {
     // 使用buffer操作符将连续的按键事件缓存为数组
     const keyCombination$ = keyDown$
       .pipe(
-        // filter((event: KeyboardEvent) => targetKeys.includes(event.key)), // 过滤目标按键
-        buffer(keyDown$.pipe(debounceTime(300))), // 在最后一个按键后等待 300ms 触发缓存
-        // filter((event: KeyboardEvent) => event.key === targetKeys[0]), // 监听第一个按键
-        // debounceTime(1000),
-        // bufferCount(3), // 在 300ms 内连续按下的按键都会被缓存
-        filter((keys: KeyboardEvent[]) => {
-          const combo = keys.map(keys => keys.key).join('')
-          console.log('pre', combo)
-          return combo === 'chun' || combo === 'xia' || combo === 'qiu' || combo === 'dong'
-          // console.log(keys)
-          // return keys.length === targetKeys.length
-        }) // 确保按键数量与目标组合键一致
+        buffer(keyDown$.pipe(debounceTime(300))) // 在最后一个按键后等待 300ms 触发缓存
+        // filter((keys: KeyboardEvent[]) => {
+        //   const combo = keys.map(keys => keys.key).join('')
+        //   return isMeetSeason(combo) !== undefined
+        // }) // 确保按键数量与目标组合键一致
       )
     // 订阅组合键事件
     const sub = keyCombination$.subscribe(comboP => {
-      // console.log('combo', combo, )
-      const combo = comboP.map(keys => keys.key).join('')
-
-      if (combo === 'chun') {
-        setRes('chun')
-        console.log('chun')
-      } else if (combo === 'xia') {
-        setRes('xia')
-        console.log('xia')
-      } else if (combo === 'qiu') {
-        setRes('qiu')
-        console.log('qiu')
-      } else if (combo === 'dong') {
-        setRes('dong')
-        console.log('dong')
-      }
+      const combo = comboP.map((keyE) => (keyE as KeyboardEvent).key).join('')
+      const season = isMeetSeason(combo)
+      setRes(season)
     })
     return () => {
       sub.unsubscribe()
     }
   }, [])
 
-  // const res = screenKey.charCodeAt(0)
   const renderText = tMap[res] || t0
-
-  // if (screenKey === 'Escape') {
-  //   renderText = t0
-  // } else {
-  //   renderText = isPressed ? tList[res % 4] : t0
-  // }
   return (
     <StyledLock className="lock" style={style}>
       <div>{renderText}</div>

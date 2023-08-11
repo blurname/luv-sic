bc12b29b2d9c1b3fa6c03d3ae3321d96ad326163
blurname <naughtybao@outlook.com>
2023-08-11 13:51:53 +0800

    CHG: split fzf code -> `fzf.ts`

--- packages/cli/src/commands/11-meta-script-fzf.ts
+++ packages/cli/src/commands/11-meta-script-fzf.ts
@@ -1,84 +1,27 @@
-import * as readline from 'node:readline'
 import { getPackageJsonFile } from '@blurname/core/src/node/meta-file/npm'
-import { spawnSync } from 'node:child_process'
-import { Fzf } from 'fzf'
-import { colorLog } from '@blurname/core/src/colorLog'
+import { createFzfKit } from '../fzf.js'
 const metaScriptFzfDesc = 'use fzf to search & execute script in project meta file '
 const metaScriptFzf = async () => {
-  const logFzfResult = ({ entries, inputStr, selectIndex }:{entries:{item:string}[], inputStr:string, selectIndex:number}) => {
-    let result = ''
-    entries.forEach((entry, index:number) => {
-      resultList.push(entry.item)
-      if (index === selectIndex) {
-        selectKey = entry.item
-        result += colorLog({ msg: `${entry.item}: ${keyDescMap.get(entry.item)}\n`, fg: 'Green' })
-      } else {
-        result += `${entry.item}: ${keyDescMap.get(entry.item)}\n`
-      }
-    }
-    )
-    const final = inputStr + '\n_______________\n' + result
-    console.clear()
-    console.log(final)
-  }
-
   const jsonFileKit = getPackageJsonFile()
   const packageJson = JSON.parse(jsonFileKit.getFileContent()) as {scripts:Record<string, string>}
 
   const scriptKeyList:string[] = []
-  const scriptDescList:string[] = []
+
   const keyDescMap = new Map(Object.entries(packageJson.scripts))
 
   Object.entries(packageJson.scripts).forEach(([k, v]) => {
     scriptKeyList.push(k)
-    scriptDescList.push(v)
   })
 
-  const fzf = new Fzf(scriptKeyList)
-  readline.emitKeypressEvents(process.stdin)
-  const inputList:string[] = []
-  let resultList: string[] = []
-
-  if (process.stdin.isTTY) { process.stdin.setRawMode(true) }
-
-  const entries = fzf.find('')
-  let selectIndex = 0
-  let selectKey = scriptKeyList[0]
-  logFzfResult({ entries, inputStr: '', selectIndex: 0 })
-
-  process.stdin.on('keypress', (str, key:{name:string, ctrl:boolean}) => {
-    console.log(key.name)
-    if ((key.ctrl === true && key.name === 'c') ||
-      key.name === 'escape') {
-      process.exit()
+  const config = {
+    msg: (item:string) => {
+      return `${item}: ${keyDescMap.get(item)}\n`
     }
-    if (key.name === 'return') {
-      spawnSync('npm', ['run', selectKey], { stdio: 'inherit' })
-      process.exit()
-    }
+  }
 
-    if (key.name === 'backspace') {
-      inputList.pop()
-      selectIndex = 0
-    } else if (key.name === 'up') {
-      if (selectIndex - 1 < 0) {
-        selectIndex = resultList.length - 1
-      } else {
-        selectIndex = (selectIndex - 1) % resultList.length
-      }
-    } else if (key.name === 'down') {
-      selectIndex = (selectIndex + 1) % resultList.length
-    } else {
-      inputList.push(str)
-      selectIndex = 0
-    }
-    const inputStr = inputList.join('')
-    const entries = fzf.find(inputStr)
-    resultList = []
-    logFzfResult({ entries, inputStr, selectIndex })
-  })
+  const fzfKit = createFzfKit({ fzfStringList: scriptKeyList, config })
+  fzfKit.runFzf()
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 export {
   metaScriptFzf, metaScriptFzfDesc
 }
--- packages/cli/src/commands/12-git-view-diff.ts
+++ packages/cli/src/commands/12-git-view-diff.ts
@@ -1,11 +1,17 @@
-import { execSync, spawnSync } from 'node:child_process'
+import { execSync, spawn, spawnSync } from 'node:child_process'
 const gitViewDiffDesc = 'git diff '
 const gitViewDiff = () => {
   const end = process.argv[3]
   const editor = process.env.EDITOR || 'vi'
 
   execSync(`git diff ${end} > .view.diff`)
-  spawnSync(editor, ['.view.diff'], { stdio: 'inherit' })
+  const viewProcess = spawn(editor, ['.view.diff'], { stdio: 'inherit' })
+  viewProcess.on('exit', () => {
+    execSync('rm .view.diff')
+  })
+
+  // const version = execSync('git log --oneline | grep VERSION -m 1').toString().split('\n')[0].split('@').at(-1)
+  // const fzf = new Fzf(scriptKeyList)
 }
 export {
   gitViewDiffDesc,

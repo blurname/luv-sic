4eefb2c0808be2507c6e1bd66a243eb1823c6ee1
blurname <naughtybao@outlook.com>
2023-08-12 16:11:18 +0800

    FIX: use react18

--- packages/lost/src/main.tsx
+++ packages/lost/src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import { RemeshRoot } from 'remesh-react'
 
-ReactDOM.render(
+const $container = document.getElementById('root')
+const root = createRoot($container)
+root.render(
   <React.StrictMode>
     <RemeshRoot>
       <App />
     </RemeshRoot>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )

1fcb22e07012378971f69fb3740be50e4bb72302
blurname <naughtybao@outlook.com>
2023-08-22 21:59:24 +0800

    ADD: page paste

--- packages/lost/src/App.tsx
+++ packages/lost/src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { RandomPress } from './pages/RandomPress'
 import { Lock } from './pages/Lock'
 import { createUrlInit } from '@blurname/core/src/browser/url'
+import { Paste } from './pages/Paste'
 const urlConfig = {
   'lock': () => 'lock',
-  'press': () => 'press'
+  'press': () => 'press',
+  'paste': () => 'paste'
 }
 const urlInit = createUrlInit(urlConfig)
 const page = urlInit()
@@ -13,6 +15,7 @@ function App () {
     <div style={{ width: '100vw', height: '100vh', boxSizing: 'border-box' }} className="App">
       {(page === undefined || page === 'lock') && <Lock/>}
       {page === 'press' && <RandomPress/>}
+      {page === 'paste' && <Paste/>}
     </div>
   )
 }
---
+++ packages/lost/src/pages/Paste.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+
+const Paste = () => {
+  const [rendertText, setRendertText] = useState('')
+  useEffect(() => {
+    const handlePaste = async (e:ClipboardEvent) => {
+      let finalT = ''
+      console.log(e.clipboardData)
+      const clipboardItems = await navigator.clipboard.read()
+
+      for (const clipboardItem of clipboardItems) {
+        for (const type of clipboardItem.types) {
+          const blob = await clipboardItem.getType(type)
+          const text = await blob.text()
+          finalT += text
+          console.log('type', type, text)
+        }
+      }
+      setRendertText(finalT)
+    }
+    document.addEventListener('paste', handlePaste)
+    return () => {
+      document.removeEventListener('paste', handlePaste)
+    }
+  }, [])
+
+  const handleCopy = async () => {
+    const obj = { hello: 'world' }
+    const blob = new Blob([JSON.stringify(obj, null, 2)], {
+      type: 'application/json'
+    })
+    await navigator.clipboard.write([
+      new ClipboardItem({
+        'web text/plain': blob
+      })
+    ])
+  }
+
+  return (
+  <div>
+  <button onClick={handleCopy}>copy</button>
+  <div>
+
+  {rendertText}
+  </div>
+  <textarea/>
+  </div>
+  )
+}
+
+export {
+  Paste
+}

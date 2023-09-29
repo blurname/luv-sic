22e9589439f24603222ce604b1c27bc74b9b0605
blurname <naughtybao@outlook.com>
2023-09-29 10:59:15 +0800

    FEAT: selectionDomain

---
+++ packages/lost/src/fake-proto/domain/selectionDomain.ts
@@ -0,0 +1,42 @@
+import { Remesh } from 'remesh'
+type Selection = string[]
+
+const SelectionDomain = Remesh.domain({
+  name: 'SelectionDomain',
+  impl: (domain) => {
+    const SelectionState = domain.state<Selection>({
+      name: 'SelectionState',
+      default: []
+    })
+
+    const SelectionQuery = domain.query({
+      name: 'SelectionQuery',
+      impl ({ get }) {
+        const selection = get(SelectionState())
+        return selection
+      }
+    })
+
+    const SelectionUpdateCommand = domain.command({
+      name: 'SelectionUpdateCommand',
+      impl (_, newSelection:Selection) {
+        return [SelectionState().new(newSelection)]
+      }
+    })
+
+    return {
+      query: {
+        SelectionQuery
+      },
+      command: {
+        SelectionUpdateCommand
+      }
+    }
+  }
+})
+export type {
+  // Buffer
+}
+export {
+  SelectionDomain
+}

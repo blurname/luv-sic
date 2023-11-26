16f220709353dcc338597851668169a812a0c7bf
blurname <naughtybao@outlook.com>
2023-11-26 15:21:09 +0800

    MOVE: package -> pkg

--- scripts/monorepo.ts
+++ scripts/monorepo.ts
@@ -1,4 +1,4 @@
-import { creteMonorepo } from '../package/monorepo/src/monorepo.js'
+import { creteMonorepo } from '../pkg/monorepo/src/monorepo.js'
 const SUB_PACKAGE_LIST = ['core', 'cli', 'svgminify', 'lost', 'monorepo']
 
 const monoRepo = creteMonorepo({ subPkgList: SUB_PACKAGE_LIST })

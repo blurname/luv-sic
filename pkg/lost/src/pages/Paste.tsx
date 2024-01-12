2d5fb87d77bfd6fa392706fcd0a25cef27897e3e
blurname <naughtybao@outlook.com>
2024-01-12 14:02:34 +0800

    CHG: paste: better display

--- pkg/lost/src/pages/Paste.tsx
+++ pkg/lost/src/pages/Paste.tsx
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from 'react'
 
+type TextList = { type:string, text: string }[]
 const Paste = () => {
-  const [rendertText, setRendertText] = useState('')
+  const [rendertTextList, setRendertTextList] = useState<TextList>([])
   useEffect(() => {
     const handlePaste = async (e:ClipboardEvent) => {
-      let finalT = ''
-      console.log(e.clipboardData)
+      const renderList:TextList = []
+      for (const item of e.clipboardData!.items) {
+        if (item.type.includes('image/png')) {
+        //
+          // const file = item.getAsFile()!
+          // const uint8Array = new Uint8Array(await file.arrayBuffer())
+          // console.log('image Uint8Array', uint8Array, String.fromCharCode(uint8Array.slice(0, 8)))
+          //
+          // console.log('image string', await file.text())
+          // renderList.push({type:'image uint8Array',)
+        //
+        }
+      }
+
       const clipboardItems = await navigator.clipboard.read()
 
       for (const clipboardItem of clipboardItems) {
         for (const type of clipboardItem.types) {
           const blob = await clipboardItem.getType(type)
           const text = await blob.text()
-          finalT += text
-          console.log('type', type, text)
+          renderList.push({ type, text })
+          if (type.includes('image')) {
+            String.fromCharCode()
+            console.log('image type', blob, new Uint8Array(blob), text)
+          }
         }
       }
-      setRendertText(finalT)
+      setRendertTextList(renderList)
     }
     document.addEventListener('paste', handlePaste)
     return () => {
@@ -24,26 +40,35 @@ const Paste = () => {
     }
   }, [])
 
-  const handleCopy = async () => {
-    const obj = { hello: 'world' }
-    const blob = new Blob([JSON.stringify(obj, null, 2)], {
-      type: 'application/json'
-    })
-    await navigator.clipboard.write([
-      new ClipboardItem({
-        'web text/plain': blob
-      })
-    ])
-  }
+  // const handleCopy = async () => {
+  //   const obj = { hello: 'world' }
+  //   const blob = new Blob([JSON.stringify(obj, null, 2)], {
+  //     type: 'application/json'
+  //   })
+  //   await navigator.clipboard.write([
+  //     new ClipboardItem({
+  //       'web text/plain': blob
+  //     })
+  //   ])
+  // }
 
   return (
   <div>
-  <button onClick={handleCopy}>copy</button>
+  {/* <button onClick={handleCopy}>copy</button> */}
   <div>
 
-  {rendertText}
+  { rendertTextList.length === 0
+    ? <h1>just paste</h1>
+
+    : rendertTextList.map((t, index) => {
+      return (
+        <div key={index}>
+        <h1>{t.type}</h1>
+        <p>{t.text}</p>
+        </div>
+      )
+    })}
   </div>
-  <textarea/>
   </div>
   )
 }

89919d419cd13cea2731ad730c106ac793aef46e
blurname <naughtybao@outlook.com>
2024-01-20 10:39:59 +0800

    CHG: color log metaScriptFzf command result

--- pkg/cli/src/commands/11-meta-script-fzf.ts
+++ pkg/cli/src/commands/11-meta-script-fzf.ts
@@ -1,6 +1,7 @@
 import { getPackageJsonFile } from '@blurname/core/src/node/meta-file/npm'
 import { createFzfKit } from '../util/fzf.js'
 import { spawnSync } from 'node:child_process'
+import { colorLog } from '@blurname/core/src/colorLog'
 const metaScriptFzfDesc = 'use fzf to search & execute script in project meta file '
 const metaScriptFzf = async () => {
   const jsonFileKit = getPackageJsonFile()
@@ -10,7 +11,7 @@ const metaScriptFzf = async () => {
 
   const keyDescMap = new Map(Object.entries(packageJson.scripts))
 
-  Object.entries(packageJson.scripts).forEach(([k, v]) => {
+  Object.entries(packageJson.scripts).forEach(([k]) => {
     scriptKeyList.push(k)
   })
 
@@ -21,7 +22,7 @@ const metaScriptFzf = async () => {
   }
   const runCallback = (selectKey:string) => {
     spawnSync('npm', ['run', selectKey], { stdio: 'inherit' })
-    console.log(`npm run ${selectKey}`)
+    colorLog({ msg: `npm run ${selectKey}`, fg: 'Green' })
   }
 
   const fzfKit = createFzfKit({ fzfStringList: scriptKeyList, config })

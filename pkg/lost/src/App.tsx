6474a375f4ec07ba5534cf385b57375a9b1a0459
blurname <naughtybao@outlook.com>
2024-03-08 20:43:42 +0800

    BREAK: use npm workspace instead of pnpm

--- pkg/lost/src/App.tsx
+++ pkg/lost/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { RandomPress } from './pages/RandomPress.js'
+import { RandomPress } from './pages/RandomPress'
 import { Lock } from './pages/Lock'
 import { createUrlInit } from '@blurname/core/src/browser/url'
-import { Paste } from './pages/Paste.js'
-import { Buffer } from './pages/Buffer.js'
-const urlConfig = {
+import { Paste } from './pages/Paste'
+// import { Buffer } from './pages/Buffer'
+import { colorLog } from '@blurname/core/src/colorLog'
+ const urlConfig = {
   'lock': () => 'lock',
   'press': () => 'press',
   'paste': () => 'paste',
@@ -13,10 +14,11 @@ const urlConfig = {
 const urlInit = createUrlInit(urlConfig)
 const page = urlInit()
 function App () {
+  colorLog({ msg: 'abcv', fg: 'Red' })
   return (
     <div style={{ width: '100vw', height: '100vh', boxSizing: 'border-box' }} className="App">
       {(page === undefined || page === 'lock') && <Lock/>}
-      {page === 'buffer' && <Buffer/>}
+      {/* {page === 'buffer' && <Buffer/>} */}
       {page === 'press' && <RandomPress/>}
       {page === 'paste' && <Paste/>}
     </div>

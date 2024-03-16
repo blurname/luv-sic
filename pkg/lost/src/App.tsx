bb49910ff45dbdcc2a1d2568c32fd043084622b7
blurname <naughtybao@outlook.com>
2024-03-16 11:30:19 +0800

    WIP

--- pkg/lost/src/App.tsx
+++ pkg/lost/src/App.tsx
@@ -4,18 +4,22 @@ import { Lock } from './page/Lock'
 import { createUrlInit } from '@blurname/core/src/browser/url'
 import { Paste } from './page/Paste'
 import { Buffer } from './page/Buffer'
+import { ElevatorEffect } from './page/ElevatorEffect'
 const urlConfig = {
   'lock': () => 'lock',
   'press': () => 'press',
   'paste': () => 'paste',
-  'buffer': () => 'buffer'
+  'buffer': () => 'buffer',
+  'effect': () => 'effect'
 }
 const urlInit = createUrlInit(urlConfig)
 const page = urlInit()
 function App () {
   return (
     <div style={{ width: '100vw', height: '100vh', boxSizing: 'border-box' }} className="App">
+
       {(page === undefined || page === 'lock') && <Lock/>}
+      {page === 'effect' && <ElevatorEffect/> }
       {page === 'buffer' && <Buffer/>}
       {page === 'press' && <RandomPress/>}
       {page === 'paste' && <Paste/>}

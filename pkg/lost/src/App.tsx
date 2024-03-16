b5ec0e1a87bb133b8f5c7533367148c7678e8e30
blurname <naughtybao@outlook.com>
2024-03-16 14:17:06 +0800

    WIP

--- pkg/lost/src/App.tsx
+++ pkg/lost/src/App.tsx
@@ -4,7 +4,7 @@ import { Lock } from './page/Lock'
 import { createUrlInit } from '@blurname/core/src/browser/url'
 import { Paste } from './page/Paste'
 import { Buffer } from './page/Buffer'
-import { EffectGround } from './page/EffectGround'
+import { RxGround } from './page/RxGround'
 const urlConfig = {
   'lock': () => 'lock',
   'press': () => 'press',
@@ -17,9 +17,8 @@ const page = urlInit()
 function App () {
   return (
     <div style={{ width: '100vw', height: '100vh', boxSizing: 'border-box' }} className="App">
-
       {(page === undefined || page === 'lock') && <Lock/>}
-      {page === 'effect' && <EffectGround/> }
+      {page === 'effect' && <RxGround/> }
       {page === 'buffer' && <Buffer/>}
       {page === 'press' && <RandomPress/>}
       {page === 'paste' && <Paste/>}
---
+++ pkg/lost/src/page/RxGround.tsx
@@ -0,0 +1,28 @@
+// https://effect.website/
+// https://effect.website/docs/introduction
+// https://effect-ts.github.io/effect/docs/effect
+
+import React, { useEffect, useState } from 'react'
+import { Elevator } from './rxbox/Elevator'
+
+export function RxGround () {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <BoxContainer title={'elevator'}>
+        <Elevator/>
+      </BoxContainer>
+      <BoxContainer title={'elevator'}>
+        <Elevator/>
+      </BoxContainer>
+    </div>
+  )
+}
+
+const BoxContainer = ({ title, children }) => {
+  return (
+    <div style={{ background: '#0022', width: '100%', outline: '1px solid #00f2', marginBottom: 10 }}>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  )
+}

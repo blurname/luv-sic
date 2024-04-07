7b59efd6e19b6c5341f84f0bf4c6327e87bb1b54
blurname <naughtybao@outlook.com>
2024-04-07 12:30:28 +0800

    DEV: monorepo: add ui

--- script/monorepo.ts
+++ script/monorepo.ts
@@ -1,5 +1,5 @@
 import { creteMonorepo } from '../pkg/monorepo/src/monorepo'
-const SUB_PACKAGE_LIST = ['core', 'cli', 'svgminify', 'lost', 'monorepo']
+const SUB_PACKAGE_LIST = ['core', 'cli', 'svgminify', 'lost', 'monorepo', 'ui']
 
 const monoRepo = creteMonorepo({ subPkgList: SUB_PACKAGE_LIST })
 monoRepo()

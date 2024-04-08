1585b2d3d80419354c4260fbfd6df8d3136ecfec
blurname <naughtybao@outlook.com>
2024-04-09 02:15:14 +0800

    CHG: monorepo: commitMsg

--- pkg/monorepo/src/version-bump.ts
+++ pkg/monorepo/src/version-bump.ts
@@ -22,32 +22,35 @@ const digitBump = (version:Version, digit:Digit) => {
 const versionBump = (subPackageList:string[]) => async (digit:Digit) => {
   const pathDir = dirname(fileURLToPath(import.meta.url)) // repoRoot/package/package/src
   const rootPath = resolve(...[pathDir, '..', '..', '..'])
-  const rootPackageJsonPath = rootPath + '/package.json'
+  // const rootPackageJsonPath = rootPath + '/package.json'
 
-  const fileKit = createFileKit(rootPackageJsonPath)
+  // const fileKit = createFileKit(rootPackageJsonPath)
 
-  let newVersion:string |undefined
+  // let newVersion:string |undefined
 
-  fileKit.modify((fileString) => {
-    const fileJson = JSON.parse(fileString)
-    fileJson.version = digitBump(fileJson.version, digit)
-    newVersion = fileJson.version
-    return JSON.stringify(fileJson, null, 2)
-  })
-  fileKit.commit()
+  // fileKit.modify((fileString) => {
+  //   const fileJson = JSON.parse(fileString)
+  //   fileJson.version = digitBump(fileJson.version, digit)
+  //   newVersion = fileJson.version
+  //   return JSON.stringify(fileJson, null, 2)
+  // })
+  // fileKit.commit()
+
+  const changedList: string[] = []
 
   for (const pkg of subPackageList) {
     const fileKit = createFileKit(rootPath + `/pkg/${pkg}/package.json`)
     fileKit.modify((fileString) => {
       const fileJson = JSON.parse(fileString)
-      fileJson.version = digitBump(fileJson.version, digit)
+      const version = digitBump(fileJson.version, digit)
+      changedList.push(`${fileJson.name}@${version}`)
+      fileJson.version = version
       return JSON.stringify(fileJson, null, 2)
     })
     fileKit.commit()
   }
 
-  const commitMsg = `VERSION: @blurname/blurkit@${newVersion}`
-  // console.log('commitMsg', commitMsg)
+  const commitMsg = `VERSION: ${changedList.join('; ')}`
   const subPackageJsonString = subPackageList.reduce((pre, cur) => `${pre} pkg/${cur}/package.json`, '')
   execSync(`git commit -i package.json ${subPackageJsonString} -m '${commitMsg}'`)
 }

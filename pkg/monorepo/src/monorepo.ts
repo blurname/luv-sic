42db9781e5df0577c69685773d97544b28b38c0b
blurname <naughtybao@outlook.com>
2024-04-13 11:39:32 +0800

    ADD: monorepo: tag-push

--- pkg/monorepo/src/monorepo.ts
+++ pkg/monorepo/src/monorepo.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'node:child_process'
 import { detectSubVersionNeedToUpdate } from './detect-sub-version.js'
 import { versionBump } from './version-bump.js'
+import { tagPush } from './tag-push.js'
 // const SUB_PACKAGE_LIST = ['core', 'cli', 'svgminify', 'lost']
 
 // 1. the sub package's root directory name must be package // TODO: bl: use arbitrary name
@@ -17,10 +18,10 @@ const creteMonorepo = ({ subPkgList, extraFunc }:CreteMonoRepoProps) => () => {
     execSync(`rm -rf dist pkg/{${pkgStr}}/dist`)
   }
 
-  const cleanNodeModules = () => {
-    const pkgStr = subPkgList.join(',')
-    execSync(`rm -rf node_modules pkg/{${pkgStr}}/node_modules`)
-  }
+  // const cleanNodeModules = () => {
+  //   const pkgStr = subPkgList.join(',')
+  //   execSync(`rm -rf node_modules pkg/{${pkgStr}}/node_modules`)
+  // }
   const cleanLock = () => {
     const pkgStr = subPkgList.join(',')
     execSync(`rm -rf pnpm-lock.yaml pkg/{${pkgStr}}/pnpm-lock.yaml`)
@@ -28,18 +29,28 @@ const creteMonorepo = ({ subPkgList, extraFunc }:CreteMonoRepoProps) => () => {
 
   const func = process.argv[2]
 
-  if (func === 'clean-dist') {
-    cleanDist()
-  } else if (func === 'clean-node-modules') {
-    cleanNodeModules()
-  } else if (func === 'version-bump') {
-    const needToBumpPkgList = detectSubVersionNeedToUpdate(subPkgList)
-    versionBump(needToBumpPkgList)('patch')
-  } else if (func === 'clean-lock') {
-    cleanLock()
-  } else {
-    if (extraFunc && typeof extraFunc[func] === 'function') {
-      extraFunc[func](subPkgList)
+  switch (func) {
+    case 'clean-dist': {
+      cleanDist()
+      break
+    }
+    case 'version-bump': {
+      const needToBumpPkgList = detectSubVersionNeedToUpdate(subPkgList)
+      versionBump(needToBumpPkgList)('patch')
+      break
+    }
+    case 'tag-push' :{
+      tagPush(subPkgList)
+      break
+    }
+    case 'clean-lock' :{
+      cleanLock()
+      break
+    }
+    default: {
+      if (extraFunc && typeof extraFunc[func] === 'function') {
+        extraFunc[func](subPkgList)
+      }
     }
   }
 }
---
+++ pkg/monorepo/src/tag-push.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+import { createFileKit } from '@blurname/core/src/node/fileKit.js'
+import { execSync } from 'node:child_process'
+
+// TODO: bl: remain git tag
+const tagPush = async (subPackageList:string[]) => {
+  const pathDir = dirname(fileURLToPath(import.meta.url))
+  const rootPath = resolve(...[pathDir, '..', '..', '..'])
+
+  // const changedList: string[] = []
+  for (const pkg of subPackageList) {
+    const subPath = rootPath + `/pkg/${pkg}`
+    const fileKit = createFileKit(rootPath + `/pkg/${pkg}/package.json`)
+    const fileString = fileKit.getFileContent()
+    const fileJson = JSON.parse(fileString)
+    if (fileJson.private) continue
+    try {
+      execSync(`cd ${subPath} && npm publish`)
+    } catch (e) {
+      console.warn(e)
+    }
+    // changedList.push(`${fileJson.name}@${fileJson.version}`)
+    // fileKit.getFileContent()
+    // fileKit.commit()
+  }
+  // execSync(`git tag ${tag}`)
+}
+
+export {
+  tagPush
+}

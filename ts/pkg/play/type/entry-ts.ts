4793d68b5a7a5284ee27036e43f7cd116133369e
blurname <naughtybao@outlook.com>
2024-07-30 22:01:50 +0800

    -n

---
+++ ts/pkg/play/type/entry-ts.ts
@@ -0,0 +1,28 @@
+const funcMap = {
+  'aaa':(a:number,aa:string)=>{
+    return 'aaa value'
+  },
+  'bbb':(b:string,bb:number)=>{
+    return 'bbb value'
+  }
+}
+
+const funcMap2 = {
+  'aaa2':(a:number,aa:string)=>{
+    return 'aaa2 value'
+  },
+  'bbb2':(b:string,bb:number)=>{
+    return 'bbb2 value'
+  }
+}
+
+const createNextMap = () => {
+  let entryMap
+
+  const setEntryMap = () => {
+
+  }
+  
+  return { setEntryMap}
+}
+
